5cd4dce6ad4d4ade05b4e7342fde82660f8d1e17
wea_ondara <wea_ondara@wea-ondara.net>
2014-12-17 13:15:19 +0100

    fixed cp errors

--- src/main/java/net/wea_ondara/sudokusolver/Sudoku.java
+++ src/main/java/net/wea_ondara/sudokusolver/Sudoku.java
@@ -16,35 +16,35 @@ public class Sudoku
     public int get(int x, int y)
     {
         Preconditions.checkPositionIndex(x, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(y, 9, "x must be between 0 and 9");
+        Preconditions.checkPositionIndex(y, 9, "y must be between 0 and 9");
         return numbers[x][y];
     }
     public void set(int x, int y, int number)
     {
         Preconditions.checkPositionIndex(x, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(y, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(number + 1, 10, "x must be between 1 and 9");
+        Preconditions.checkPositionIndex(y, 9, "y must be between 0 and 9");
+        Preconditions.checkPositionIndex(number + 1, 10, "number must be between 1 and 9");
         
         numbers[x][y] = number;
     }
     public boolean[] getNotes(int x, int y)
     {
         Preconditions.checkPositionIndex(x, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(y, 9, "x must be between 0 and 9");
+        Preconditions.checkPositionIndex(y, 9, "y must be between 0 and 9");
         return notes[x][y];
     }
     public void setNote(int x, int y, int number, boolean set)
     {
         Preconditions.checkPositionIndex(x, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(y, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(number + 1, 10, "x must be between 1 and 9");
+        Preconditions.checkPositionIndex(y, 9, "y must be between 0 and 9");
+        Preconditions.checkPositionIndex(number + 1, 10, "number must be between 1 and 9");
         
         notes[x][y][number] = set;
     }
     public void clearNotes(int x, int y)
     {
         Preconditions.checkPositionIndex(x, 9, "x must be between 0 and 9");
-        Preconditions.checkPositionIndex(y, 9, "x must be between 0 and 9");
+        Preconditions.checkPositionIndex(y, 9, "y must be between 0 and 9");
         notes[x][y] = new boolean[9];
     }
 }
